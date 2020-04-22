b3f00fbff218e91b1141807c46fab6fb98d444e7
Sarim-K <shktv03@gmail.com>
2020-04-22 14:59:33 +0100

    implemented setup

--- main.py
+++ main.py
@@ -30,30 +30,36 @@ async def on_guild_join(guild):
 
     sql_query = f"""
     CREATE TABLE IF NOT EXISTS settings_{server_id} (
-    UPVOTE_ID   INTEGER (0, 55),
-    RT_ID   INTEGER (0, 55),
-    DOWNVOTE_ID INTEGER (0, 55)
+    UPVOTE_ID   STRING (0, 56),
+    RT_ID   STRING (0, 56),
+    DOWNVOTE_ID STRING (0, 56)
     );"""
     c.execute(sql_query)
 
     for channel in guild.text_channels:
         if channel.permissions_for(guild.me).send_messages:
-            await channel.send("""Hello! To get started, type out your desired reaction emotes as such:
-$setup <:upvote:452121917462151169> <:rt:451882250884218881> <:downvote:451890347761467402>""")
+            await channel.send("Hello! To get started, type out your desired reaction emotes as such:\n$setup <:upvote:452121917462151169> <:rt:451882250884218881> <:downvote:451890347761467402>")
             break
 
 
 @commands.has_permissions(administrator=True)
 @bot.command()
 async def setup(ctx):
-    server_id = str(ctx.guild.id)
-    user_id = str(ctx.author.id)
+    upvote_id = ctx.message.content.split(" ")[1]
+    downvote_id = ctx.message.content.split(" ")[2]
+    rt_id = ctx.message.content.split(" ")[3]
+    print(upvote_id, downvote_id, rt_id)
 
-    if str(ctx.author.id) == user_id:
-        upvote_id = ctx.message.content.split(" ")[1]
-        downvote_id = ctx.message.content.split(" ")[2]
-        rt_id = ctx.message.content.split(" ")[3]
-        print(upvote_id, downvote_id, rt_id, server_id)
+    sql_query = f"DELETE FROM settings_{ctx.guild.id}"
+    c.execute(sql_query)
+    conn.commit()
+
+    sql_query = f"""
+    INSERT INTO settings_{ctx.guild.id}
+    VALUES('{upvote_id}', '{rt_id}', '{downvote_id}'
+    );"""
+    c.execute(sql_query)
+    conn.commit()
 
 
 bot.run(KEY)
