32f9b0ac64ed56d8c2e23c679d9f8b114438b03a
Sarim-K <shktv03@gmail.com>
2020-05-14 19:59:36 +0100

    customise cmd & img support

--- help_cmds.py
+++ help_cmds.py
@@ -2,13 +2,14 @@ import discord
 
 
 class createHelpMessage:
-	def __init__(self, title, description, colour):
+	def __init__(self, title, description, colour, img=None):
 		self.title = title
 		self.description = description
 		self.colour = colour
+		self.img = img 
 
 
-def help(usermessage, message):
+def help(usermessage, message, img=None):
 	if usermessage == "$help setup":
 		help_message = createHelpMessage("ADMIN ONLY: used to choose server's voting emotes; type 'NONE' if a specific emote isnt required", "$setup <:upvote:452121917462151169> <:rt:451882250884218881> <:downvote:451890347761467402>", 1)
 
@@ -30,10 +31,17 @@ def help(usermessage, message):
 	elif usermessage == "$help given":
 		help_message = createHelpMessage("view how much karma a user's given", "$given or $given @user", 1)	
 
+	elif usermessage == "$help customise":
+		img = discord.File("karma_card/help.png")
+		help_message = createHelpMessage("$customise [karma/given] [template name] [dark/light]", "$customise karma blacksea dark", 1)
+
 	else:  # catch-all help command
-		help_message = createHelpMessage("commands:", "$setup\n$debug\n$blacklist_add\n$blacklist_remove\n$blacklist_view\n\n$karma\n$given", 1)
+		help_message = createHelpMessage("commands:", "$setup\n$debug\n$blacklist_add\n$blacklist_remove\n$blacklist_view\n\n$karma\n$given\ncustomise", 1)
 
 	embed = discord.Embed(title=help_message.title, description=help_message.description, color=help_message.colour)
 	embed.set_author(name=message.author, icon_url=message.author.avatar_url)
-	return embed
 
+	if img is None:
+		return embed
+	else:
+		return embed, img
